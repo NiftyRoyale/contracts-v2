6bb5a912c62d22375463987f1cbf11c9eb02fd79
Jack <jackjin701@gmail.com>
2021-10-25 10:35:02 +0800

    RandomNumberGenerator Integrate

---
+++ truffle-config.js
@@ -0,0 +1,38 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const secretMumbai = require("./secret.mumbai.json");
+
+module.exports = {
+  // Uncommenting the defaults below
+  // provides for an easier quick-start with Ganache.
+  // You can also follow this format for other networks;
+  // see <http://truffleframework.com/docs/advanced/configuration>
+  // for more details on how to specify configuration options!
+  //
+  networks: {
+    mumbai: {
+      provider: () => new HDWalletProvider(secretMumbai.mnemonic, `https://rpc-mumbai.maticvigil.com`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 5000,
+      skipDryRun: true
+    },
+    matic: {
+      provider: () => new HDWalletProvider(secretMumbai.mnemonic, `https://rpc-mainnet.maticvigil.com`),
+      network_id: 137,
+      confirmations: 2,
+      timeoutBlocks: 5000,
+      skipDryRun: true
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.8.6",
+    },
+  },
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+  api_keys: {
+    etherscan: secretKovan.etherscan_api_key,
+  },
+};
